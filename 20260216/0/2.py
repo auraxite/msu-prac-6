9a2bc100d171ed8c2c21db28feae02c45c7181a6
Kirill <kokibizov@gmail.com>
2026-02-16 14:23:55 +0300

    20260216 cw 3

--- 20260216/0/2.py
+++ 20260216/0/2.py
@@ -1,8 +1,6 @@
-#!/usr/bin/share python3
-
 import sys
 from pathlib import Path
 
-for p in Path(sys.argv[1], '.git/objects').glob('**/*'):
-    if p.is_file() and p.parent.name not in ('info', 'pack'):
+for p in Path(sys.argv[1], ".git/objects").glob("**/*"):
+    if p.is_file() and p.parent.name not in ("info", "pack"):
         print(p.parent.name + p.name)
